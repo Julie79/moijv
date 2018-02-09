c8b17bfed886d35118dfa4a0be66e0635e04b938
Julie <juliegalland@yahoo.fr>
2018-02-09 10:10:37 +0100

    Add product form and my_products page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,142 +5,185 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-// La ligne ci-dessous permet de lier User à UserRepository, et donc à UserController!
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- */
-class User
+* @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+*/
+class User implements UserInterface, \Serializable
 {
-    // CHAMPS
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-    
-    // column(type=string) renseigne le type de colonne pour Doctrine
-    // @var string renseigne le type de colonne pour Symfony
-    /**
-     * @ORM\column(type="string", length=100, nullable=true)
-     * @var string 
-     */
-    private $lastname;
-    
-    /**
-     * @ORM\column(type="string", length=100, nullable=true)
-     * @var string 
-     */
-    private $firstname;
-    
-    /**
-     * @ORM\column(type="string", length=255)
-     * @var string
-     */
-    private $email;
-    
-    /**
-     * @ORM\column(type="string", length=255)
-     * @var string
-     */
-    private $password;
-    
-    /**
-     * @ORM\column(type="string", length=100)
-     * @var string
-     */
-    private $username;
-    
-    /**
-     * @ORM\column(type="date")
-     * @var \DateTime
-     */
-    private $birthdate;
-    
-    /**
-     * @ORM\OneToMany(targetEntity="Product", mappedBy="user")
-     * @var Collection
-     */
-    private $products;
-    
-    
-    public function __construct() {
-        $this->products = new ArrayCollection();
-    }
-        
-        
-    // GETTERS
-    public function getId() {
-        return $this->id;
-    }
+   /**
+    * @ORM\Id
+    * @ORM\GeneratedValue
+    * @ORM\Column(type="integer")
+    */
+   private $id;
 
-    public function getLastname() {
-        return $this->lastname;
-    }
+   /**
+    * @ORM\Column(type="string", length=100, nullable=true)
+    * @var string
+    */
+   private $lastname;
+   
+   /**
+    * @ORM\Column(type="string", length=100, nullable=true)
+    * @var string
+    */
+   private $firstname;
+   
+   /**
+    * @ORM\Column(type="string", length=255)
+    * @var string
+    */
+   private $email;
+   
+   /**
+    * @ORM\Column(type="string", length=255)
+    * @var string
+    */
+   private $password;
+   
+   /**
+    * @ORM\Column(type="string", length=100)
+    * @var string
+    */
+   private $username;
+   
+   /**
+    * @ORM\Column(type="date")
+    * @var \DateTime
+    */
+   private $birthdate;
+   
+   /**
+    * @ORM\OneToMany(targetEntity="Product", mappedBy="user")
+    * @var Collection
+    */
+   private $products;
+   
+   /**
+    * @ORM\Column(type="string")
+    * @var string
+    */
+   private $roles;
+       
+   public function __construct() {
+       $this->products = new ArrayCollection();
+   }
+   
+   public function getSalt()
+   {
+       // you *may* need a real salt depending on your encoder
+       // see section on salt below
+       return null;
+   }
+   
+    public function getRoles()
+   {
+       return explode(',', $this->roles);
+   }
+   
+    public function eraseCredentials()
+   {
+   }
+   
+    /** @see \Serializable::serialize() */
+   public function serialize()
+   {
+       return serialize(array(
+           $this->id,
+           $this->username,
+           $this->password,
+       ));
+   }
 
-    public function getFirstname() {
-        return $this->firstname;
-    }
+   /** @see \Serializable::unserialize() */
+   public function unserialize($serialized)
+   {
+       list (
+           $this->id,
+           $this->username,
+           $this->password,
+       ) = unserialize($serialized);
+   }
 
-    public function getEmail() {
-        return $this->email;
-    }
+   
+   public function getProducts(): Collection {
+       return $this->products;
+   }
 
-    public function getPassword() {
-        return $this->password;
-    }
+   public function setProducts(Collection $products) {
+       $this->products = $products;
+       return $this;
+   }
 
-    public function getUsername() {
-        return $this->username;
-    }
+       
+   public function getId() {
+       return $this->id;
+   }
 
-    public function getBirthdate(): \DateTime {
-        return $this->birthdate;
-    }
-    
-    public function getProducts(): Collection {
-        return $this->products;
-    }
+   public function getLastname() {
+       return $this->lastname;
+   }
 
-    // SETTERS
-    public function setId($id) {
-        $this->id = $id;
-        return $this;
-    }
+   public function getFirstname() {
+       return $this->firstname;
+   }
 
-    public function setLastname($lastname) {
-        $this->lastname = $lastname;
-        return $this;
-    }
+   public function getEmail() {
+       return $this->email;
+   }
 
-    public function setFirstname($firstname) {
-        $this->firstname = $firstname;
-        return $this;
-    }
+   public function getPassword() {
+       return $this->password;
+   }
 
-    public function setEmail($email) {
-        $this->email = $email;
-        return $this;
-    }
+   public function getUsername() {
+       return $this->username;
+   }
 
-    public function setPassword($password) {
-        $this->password = $password;
-        return $this;
-    }
+   public function getBirthdate(): \DateTime {
+       return $this->birthdate;
+   }
 
-    public function setUsername($username) {
-        $this->username = $username;
-        return $this;
-    }
+   public function setId($id) {
+       $this->id = $id;
+       return $this;
+   }
 
-    public function setBirthdate(\DateTime $birthdate) {
-        $this->birthdate = $birthdate;
-        return $this;
-    }
+   public function setLastname($lastname) {
+       $this->lastname = $lastname;
+       return $this;
+   }
 
-    public function setProducts(Collection $products) {
-        $this->products = $products;
-        return $this;
-    }
+   public function setFirstname($firstname) {
+       $this->firstname = $firstname;
+       return $this;
+   }
+
+   public function setEmail($email) {
+       $this->email = $email;
+       return $this;
+   }
+
+   public function setPassword($password) {
+       $this->password = $password;
+       return $this;
+   }
+
+   public function setUsername($username) {
+       $this->username = $username;
+       return $this;
+   }
+
+   public function setBirthdate(\DateTime $birthdate) {
+       $this->birthdate = $birthdate;
+       return $this;
+   }
+   
+    function setRoles($roles) {
+       $this->roles = $roles;
+   }
+
+
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ProductRepository extends ServiceEntityRepository
@@ -25,16 +26,20 @@ class ProductRepository extends ServiceEntityRepository
     }
 
     // fonction pour trouver tous les produits associés à un tag
-    public function findByTagWithTags($tag) {
-        
-        return $this->createQueryBuilder('p')
-                // p.tags=join t=alias
-                ->leftJoin('p.tags', 't')
-                ->addSelect('t')
-                ->where('t.id = :id')
-                ->setParameter(':id', $tag->getId())
-                ->getQuery()
-                ->getResult();
-    }
+   public function findByTagWithTags($tag) {
+       // SELECT p.*, t.* FROM product p
+       // LEFT JOIN product_tag ON product_tag.product_id = product.id
+       // LEFT JOIN tag t ON tag.id = product_tag.tag_id
+       // WHERE tag.id = 25
+       return $this->createQueryBuilder('p')
+               ->lefJoin('p.tags', 't')
+               ->leftJoin(Product::class, 'p2', Join::WITH, 'p2.id = p.id')
+               ->leftJoin('p2.tags', 't2')
+               ->addSelect('t')
+               ->where('t.id = :id')
+               ->setParameter(':id', $tag->getId())
+               ->getQuery()
+               ->getResult();
+   }
     
 }
